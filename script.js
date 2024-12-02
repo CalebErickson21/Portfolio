41b9f7dc4af17a08e88f9fef64a15da9abb2da45
Caleb Erickson <coding.c9@gmail.com>
2024-12-02 22:22:57 +0000

    Added email functionality

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-document.getElementById('resumeDownload').addEventListener('click', () => {
+function resumeDownload (){
     const link = document.createElement('a');
     link.href = 'https://github.com/CalebErickson21/portfolio/raw/refs/heads/main/downloadables/Caleb%20Erickson%20Resume.docx'; // Replace with your local server URL
     link.download = 'Caleb_Erickson_Resume.docx'; // Suggested filename for the user
     link.click();
-  });
+};
+
+function sendEmail() {
+  const subject = encodeURIComponent(document.getElementById('emailSubject').value);
+  const body = encodeURIComponent(document.getElementById('emailBody').value);
+  window.location.href = `mailto:crerickson@crimson.ua.edu?subject=${subject}&body=${body}`;
+}
