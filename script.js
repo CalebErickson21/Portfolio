aa435d71bf1abe06e568fa1037061308728b0ae1
Caleb Erickson <coding.c9@gmail.com>
2024-12-03 03:37:30 +0000

    changed typos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function resumeDownload (){
     const link = document.createElement('a');
-    link.href = 'https://github.com/CalebErickson21/portfolio/raw/refs/heads/main/downloadables/Caleb%20Erickson%20Resume.docx'; // Replace with your local server URL
-    link.download = 'Caleb_Erickson_Resume.docx'; // Suggested filename for the user
+    link.href = 'https://github.com/CalebErickson21/portfolio/raw/refs/heads/main/downloadables/Caleb%20Erickson%20Resume.docx';
+    link.download = 'Caleb_Erickson_Resume.docx';
     link.click();
 };
 
