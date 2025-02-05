0520c9ac6b87281da4b55e82452ac94029485fc0
Caleb Erickson <coding.c9@gmail.com>
2025-02-05 02:43:18 +0000

    Centered nav tabs the correct way

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const mdWidth = 768;
+const smWidth = 576;
+const lgWidth = 992;
+
+
 function resumeDownload (){
     const link = document.createElement('a');
     link.href = 'https://github.com/CalebErickson21/portfolio/raw/refs/heads/main/downloadables/Caleb%20Erickson%20Resume.docx';
@@ -5,8 +10,20 @@ function resumeDownload (){
     link.click();
 };
 
+
 function sendEmail() {
   const subject = encodeURIComponent(document.getElementById('emailSubject').value);
   const body = encodeURIComponent(document.getElementById('emailBody').value);
   window.location.href = `mailto:crerickson@crimson.ua.edu?subject=${subject}&body=${body}`;
 }
+
+
+function toggleClassOnResize(element, toggleClass, size) {
+  if (window.innerWidth < size) { element?.classList.remove(toggleClass); }
+  else { element?.classList.add(toggleClass); }
+}
+
+// Navbar dynamic centering
+const navCenter = document.querySelector("#navbar-center");
+window.addEventListener("resize", () => toggleClassOnResize(navCenter, "navbar-center", lgWidth));
+window.addEventListener("load", () => toggleClassOnResize(navCenter, "navbar-center", lgWidth));
