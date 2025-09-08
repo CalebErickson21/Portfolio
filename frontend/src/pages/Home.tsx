38a5eb028d231c69bd4193510dab2505d1c4a61d
Caleb Erickson <coding.c9@gmail.com>
2025-09-08 10:48:23 -0500

    intial setup and routing complete using react router and a routes.tsx file. This is different than the basic App.tsx with the <Router></Router> component setup. This way offers more flexibility and granular control. No need for an App.tsx or App.css with this setup.

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,25 @@
+// Import dependencies
+
+
+// Import components
+
+
+// Import styles
+
+
+// Home page component
+const Home = () => {
+
+
+    return (
+        <>
+            <div>
+                <h1>Home</h1>
+            </div>
+        </>
+    )
+
+
+}
+
+export default Home;
