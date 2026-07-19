b0638c9f134ffb9a5b02832b394c5bab825b73b8
Caleb Erickson <coding.c9@gmail.com>
2026-07-18 20:20:46 -0500

    Clear out old repo

--- frontend/src/contexts/appProviders.tsx
+++ frontend/src/contexts/appProviders.tsx
@@ -1,7 +1,6 @@
 // Import providers
-import { EnvironmentProvider } from "@/contexts/environment";
 
 // App providers component
 export const AppProviders = ({ children }: { children: React.ReactNode }) => {
-    return <EnvironmentProvider>{children}</EnvironmentProvider>;
+    return { children };
 };
--- frontend/src/utils/routes.tsx
+++ frontend/src/utils/routes.tsx
@@ -1,13 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/pages/layout";
 import Home from "@/pages/home";
-import About from "@/pages/about";
-import Experience from "@/pages/experience";
-import Projects from "@/pages/projects";
-import Academics from "@/pages/academics";
-import Contact from "@/pages/contact";
-import Construction from "@/pages/construction";
-import Publications from "@/pages/publications";
 
 const router = createBrowserRouter([
     {
@@ -17,34 +10,6 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <Home />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "/experience",
-                element: <Experience />,
-            },
-            {
-                path: "/projects",
-                element: <Projects />,
-            },
-            {
-                path: "/academics",
-                element: <Academics />,
-            },
-            {
-                path: "/contact",
-                element: <Contact />,
-            },
-            {
-                path: "/construction",
-                element: <Construction />,
-            },
-            {
-                path: "/publications",
-                element: <Publications />,
             },
         ],
     },
